45fc6e4eda5928b925f80362f6e078c7fe044f9a
kashyapAJ <100838228+kashyapAJ@users.noreply.github.com>
2022-03-04 15:51:46 +0530

    Add files via upload

---
+++ practical/queue/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Create and initialize a queue
+          Queue que = new Queue();
+            que.Enqueue("Welcome");
+            que.Enqueue("Ajay kumar ");
+            que.Enqueue(20.5f);
+            que.Enqueue(10);
+            que.Enqueue(100);
+            Console.WriteLine("***Queue Example***");
+            Console.WriteLine("Number of Elements in Queue: {0}", que.Count);
+            Console.WriteLine("***Queue Elements***");
+            // Access Queue Elements
+            foreach (var item in que)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
