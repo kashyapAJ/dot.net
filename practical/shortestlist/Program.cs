68bf09843f05a3e36a524641c3ab7fc79f00a695
kashyapAJ <100838228+kashyapAJ@users.noreply.github.com>
2022-03-04 15:45:33 +0530

    Add files via upload

---
+++ practical/shortestlist/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList sl = new SortedList();
+            sl.Add("ora", "oracle");
+            sl.Add("vb", "vb.net");
+            sl.Add("cs", "cs.net");
+            sl.Add("asp", "asp.net");
+
+            foreach (DictionaryEntry d in sl)
+            {
+                Console.WriteLine(d.Key + " " + d.Value);
+                Console.WriteLine("");
+            }
+        }
+    }
+}
