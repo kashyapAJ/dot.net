53a303b020ab249347f64f7581fc1477e45a9a77
kashyapAJ <100838228+kashyapAJ@users.noreply.github.com>
2022-03-14 11:25:58 +0530

    Add files via upload

---
+++ RegistrationFORM/RegistrationForm.aspx
@@ -0,0 +1,91 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RegistrationForm.aspx.cs" Inherits="userinfo.RegistrationForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+<link href="Content/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+<form id="form1" runat="server">
+<div style="padding:5px;">
+<h2>User Registration Form :-</h2>
+<table class="table table-condensed" style="width:500px;">
+<tr>
+<td>
+Name:
+</td>
+<td>
+    <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+</td>
+</tr>
+<tr>
+<td>
+Phone No. :
+</td>
+<td>
+    <asp:TextBox ID="txtNumber" runat="server"></asp:TextBox>
+</td>
+</tr>
+<tr>
+<td>
+Gender :
+</td>
+<td>
+    <asp:RadioButtonList ID="Gender" runat="server">
+        <asp:ListItem>Male</asp:ListItem>
+        <asp:ListItem>Female</asp:ListItem>
+        <asp:ListItem>Others</asp:ListItem>
+    </asp:RadioButtonList>
+</td>
+</tr>
+<tr>
+<td>
+City :
+</td>
+<td>
+    <asp:DropDownList ID="City" runat="server">
+        <asp:ListItem>Select Your City</asp:ListItem>
+        <asp:ListItem>Delhi</asp:ListItem>
+        <asp:ListItem>Bihar</asp:ListItem>
+        <asp:ListItem>UP</asp:ListItem>
+        <asp:ListItem>J & K</asp:ListItem>
+        <asp:ListItem>Assam</asp:ListItem>
+        <asp:ListItem>Haryana</asp:ListItem>
+        <asp:ListItem>Punjab</asp:ListItem>
+        <asp:ListItem>Karnataka</asp:ListItem>
+        <asp:ListItem>Gujrat</asp:ListItem>
+    </asp:DropDownList>
+</td>
+</tr>
+    <tr>
+<td>
+Email ID :
+</td>
+<td>
+    <asp:TextBox ID="txtemail" runat="server"></asp:TextBox>
+</td>
+</tr>
+<tr>
+<td>
+Create Password :
+</td>
+<td>
+    <asp:TextBox ID="txtpassword" runat="server"></asp:TextBox>
+</td>
+</tr>
+<tr>
+<td colspan="2" align="left">
+    <asp:Button ID="btn_submit" runat="server" Text="Submit" OnClick="btnsubmit_Click" />
+</td>
+</tr>
+</table>
+<asp:Label runat="server" ID="lblInfo" />
+
+<br /><br />
+    <asp:GridView ID="proDataGrid" runat="server" CssClass="table table-condensed" Width="500px"/>
+</div>
+</form>
+</body>
+</html>
---
+++ RegistrationFORM/RegistrationForm.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace userinfo
+{
+    public partial class RegistrationForm : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-9PML351\SQLEXPRESS; Initial Catalog=User; Integrated Security=True");
+        SqlCommand cmd;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            {
+                if (!IsPostBack)
+                    FillGrid();
+            }
+        }
+
+
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtName.Text == null || txtNumber.Text == null || City.SelectedValue == null || Gender.Text == null ||
+                    txtemail.Text == null || txtpassword.Text == null ||// add more coloum
+                txtName.Text.ToString().Trim().Equals("") || txtNumber.Text.ToString().Trim().Equals("") || Gender.Text.ToString().Trim().Equals("") || City.Text.ToString().Trim().Equals("") || txtpassword.Text.ToString().Trim().Equals("") || txtemail.Text.ToString().Trim().Equals(""))
+                    lblInfo.Text = "Please enter all fields";
+                else
+                {
+
+                    cmd = new SqlCommand();
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.CommandText = "sp_user";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@UName", txtName.Text.ToString());
+                    cmd.Parameters.AddWithValue("@Contact_Number", txtNumber.Text.ToString());
+                    cmd.Parameters.AddWithValue("@Gender", Gender.SelectedItem.Text);
+                    cmd.Parameters.AddWithValue("@City", City.SelectedValue);
+                    cmd.Parameters.AddWithValue("@Email_ID", txtemail.Text.ToString());
+                    cmd.Parameters.AddWithValue("@C_Password", txtpassword.Text.ToString());// add more parameter for coloum
+                    cmd.ExecuteNonQuery();
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(new SqlCommand("Select UName, Contact_Number,Gender,City,Email_ID,C_Password from User_Data", con));
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    proDataGrid.DataSource = dt;
+                    proDataGrid.DataBind();
+                    adapter.Dispose();
+                    cmd.Dispose();
+                    con.Close();
+
+                    lblInfo.Text = "Added Successfully";
+                    txtName.Text = "";
+                    txtNumber.Text = "";
+                    txtpassword.Text = "";
+                    txtemail.Text = "";
+                    City.Text = "";
+                    Gender.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblInfo.Text = "Error:" + ex.Message.ToString();
+            }
+        }
+
+
+        public void FillGrid()
+        {
+            try
+            {
+
+                con.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(new SqlCommand("Select UName, Contact_Number,Gender,City,Email_ID,C_Password from User_Data", con)); ///
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                proDataGrid.DataSource = dt;
+                proDataGrid.DataBind();
+                adapter.Dispose();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
