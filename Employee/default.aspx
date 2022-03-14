754e94a189acf96df38c746189c61a92495b813b
kashyapAJ <100838228+kashyapAJ@users.noreply.github.com>
2022-03-14 11:28:36 +0530

    Add files via upload

---
+++ Employee/default.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="Employee._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="Content/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+<form id="form1" runat="server">
+<div style="padding:5px;">
+<h2>Add Data using SQL Stored Procedure</h2>
+<table class="table table-condensed" style="width:500px;">
+<tr>
+<td>
+Employee Name:
+</td>
+<td>
+<asp:TextBox runat="server" ID="txtEmpName" placeholder="Enter Employee Name"
+Width="150px"/>
+</td>
+</tr>
+<tr>
+<td>
+Employee Designation:
+</td>
+<td>
+<asp:TextBox TextMode="MultiLine" runat="server" ID="txtEmpDes"
+Width="150px" placeholder="Enter Employee Designation"/>
+</td>
+</tr>
+<tr>
+<td colspan="2" align="left">
+<asp:Button runat="server" Text="Submit" ID="btnSubmit" OnClick="btnSubmit_Click"/>
+</td>
+</tr>
+</table>
+<asp:Label runat="server" ID="lblInfo" />
+
+<br /><br />
+<asp:GridView runat="server" ID="proDataGrid" CssClass="table table-condensed" Width="500px"/>
+</div>
+</form>
+</body>
+</html>
---
+++ Employee/default.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Employee
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-9PML351\SQLEXPRESS; Initial Catalog=EMPL; Integrated Security=True");
+        SqlCommand cmd;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                FillGrid();
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtEmpDes.Text == null || txtEmpName.Text == null ||
+                txtEmpDes.Text.ToString().Trim().Equals("") || txtEmpName.Text.ToString().Trim().Equals(""))
+                    lblInfo.Text = "Please enter all fields";
+                else
+                {
+                   
+                    cmd = new SqlCommand();
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.CommandText = "sp_EMPL";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Emp_Name", txtEmpName.Text.ToString());
+                    cmd.Parameters.AddWithValue("@Emp_des", txtEmpDes.Text.ToString());
+                    cmd.ExecuteNonQuery();
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(new SqlCommand("Select Emp_Name,Emp_Des,OnDate from Employees_DB", con));
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    proDataGrid.DataSource = dt;
+                    proDataGrid.DataBind();
+                    adapter.Dispose();
+                    cmd.Dispose();
+                    con.Close();
+
+                    lblInfo.Text = "Added Successfully";
+                    txtEmpDes.Text = "";
+                    txtEmpName.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblInfo.Text = "Error:" + ex.Message.ToString();
+            }
+        }
+
+        public void FillGrid()
+        {
+            try
+            {
+                
+                con.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(new SqlCommand("Select Emp_Name,Emp_Des,OnDate from Employees_DB", con));
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                proDataGrid.DataSource = dt;
+                proDataGrid.DataBind();
+                adapter.Dispose();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
