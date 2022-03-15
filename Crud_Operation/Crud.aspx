f437c7dba0ed2a3f3baae1572b81655ca7955ccb
kashyapAJ <100838228+kashyapAJ@users.noreply.github.com>
2022-03-15 13:48:36 +0530

    Add files via upload

---
+++ Crud_Operation/Crud.aspx
@@ -0,0 +1,190 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Crud.aspx.cs" Inherits="Crud_Operation.Crud" %>
+
+<!DOCTYPE html>
+<script runat="server">
+
+
+
+  
+</script>
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head runat="server">
+
+    <title></title>
+
+    <style type="text/css">
+
+        .auto-style1 {
+
+            border: 3px solid #FF6600;
+
+            background-color: #CCFF99;
+
+        }
+
+        .auto-style4 {
+
+            width: 543px;
+
+        }
+
+        .auto-style6 {
+
+            width: 381px;
+
+            text-align: right;
+
+        }
+
+        .auto-style7 {
+
+            width: 543px;
+
+            text-align: left;
+
+            height: 76px;
+
+        }
+
+        .auto-style8 {
+
+            width: 381px;
+
+            text-align: right;
+
+            height: 76px;
+
+        }
+
+    </style>
+
+</head>
+
+<body>
+
+    <form id="form1" runat="server">
+
+        <div>
+
+ 
+
+            <table cellpadding="2" class="auto-style1">
+
+                <tr>
+
+                    <td class="auto-style6">Id</td>
+
+                    <td class="auto-style4">
+
+                        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+
+                    &nbsp;&nbsp;&nbsp;
+
+                        <asp:Button ID="btnsearch" runat="server" OnClick="btnsearch_Click" Text="Search" />
+
+&nbsp;&nbsp;&nbsp;
+
+                        <asp:Label ID="Label2" runat="server"></asp:Label>
+
+                    </td>
+
+                </tr>
+
+                <tr>
+
+                    <td class="auto-style6">Name</td>
+
+                    <td class="auto-style4">
+
+                        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+
+                    </td>
+
+                </tr>
+
+                <tr>
+
+                    <td class="auto-style6">Salary</td>
+
+                    <td class="auto-style4">
+
+                        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+
+                    </td>
+
+                </tr>
+
+                <tr>
+
+                    <td class="auto-style6">Address</td>
+
+                    <td class="auto-style4">
+
+                        <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+
+                    </td>
+
+                </tr>
+
+                <tr>
+
+                    <td class="auto-style8">
+
+                        <asp:Button ID="btnInsert" runat="server" Height="38px" OnClick="btnInsert_Click" Text="Insert" Width="97px" />
+
+                    </td>
+
+                    <td class="auto-style7">
+
+                        <asp:Button ID="btnupdate" runat="server" Height="31px" OnClick="btnupdate_Click" Text="Update" />
+
+&nbsp;
+
+                        <asp:Button ID="btndelete" runat="server" Height="31px" Text="Delete" OnClick="btndelete_Click" />
+
+&nbsp;<asp:Button ID="btnviews" runat="server" Text="Views" OnClick="btnviews_Click" />
+
+&nbsp; </td>
+
+                </tr>
+
+            </table>
+
+ 
+
+        </div>
+
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+
+        <asp:GridView ID="GridView1" runat="server" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="3">
+
+            <FooterStyle BackColor="White" ForeColor="#000066" />
+
+            <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+
+            <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+
+            <RowStyle ForeColor="#000066" />
+
+            <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+
+            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+
+            <SortedAscendingHeaderStyle BackColor="#007DBB" />
+
+            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+
+            <SortedDescendingHeaderStyle BackColor="#00547E" />
+
+        </asp:GridView>
+
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server"></asp:SqlDataSource>
+
+    </form>
+
+</body>
+
+</html>
---
+++ Crud_Operation/Crud.aspx.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Crud_Operation
+{
+    public partial class Crud : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-9PML351\SQLEXPRESS; Initial Catalog=Crud_Operation; Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            btnupdate.Enabled = false;
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+
+            {
+
+                
+
+                SqlCommand cmd = new SqlCommand("Insert into Employee1 values(@id,@name,@salary,@address)", con);
+
+                cmd.CommandType = CommandType.Text;
+
+                con.Open();
+
+                cmd.Parameters.AddWithValue("@id", TextBox1.Text);
+
+                cmd.Parameters.AddWithValue("@name", TextBox2.Text);
+
+                cmd.Parameters.AddWithValue("@salary", TextBox3.Text);
+
+                cmd.Parameters.AddWithValue("@address", TextBox4.Text);
+
+                cmd.ExecuteNonQuery();
+
+                Label1.Text = "Data has been saved";
+
+                con.Close();
+
+                TextBox1.Text = "";
+
+                TextBox2.Text = "";
+
+                TextBox3.Text = "";
+
+                TextBox4.Text = "";
+
+            }
+
+            catch
+
+            {
+
+                Label1.Text = "First Search Data";
+
+            }
+        }
+
+        protected void btnupdate_Click(object sender, EventArgs e)
+        {
+            
+
+            SqlCommand cmd = new SqlCommand("Update Employee1 set name=@name,salary=@salary,address=@address where id=@id", con);
+
+            cmd.CommandType = CommandType.Text;
+
+            con.Open();
+
+
+
+            cmd.Parameters.AddWithValue("@name", TextBox2.Text);
+
+            cmd.Parameters.AddWithValue("@salary", TextBox3.Text);
+
+            cmd.Parameters.AddWithValue("@address", TextBox4.Text);
+
+            cmd.Parameters.AddWithValue("@id", TextBox1.Text);
+
+
+
+
+
+            cmd.Connection = con;
+
+            cmd.ExecuteNonQuery();
+
+            Label1.Text = "Data has been Updated";
+
+            con.Close();
+
+            TextBox1.Text = "";
+
+            TextBox2.Text = "";
+
+            TextBox3.Text = "";
+
+            TextBox4.Text = "";
+
+
+
+
+
+        }
+
+        protected void btndelete_Click(object sender, EventArgs e)
+        {
+            try
+
+            {
+
+                
+
+
+
+                SqlCommand cmd = new SqlCommand("delete from Employee1 where id=" + TextBox1.Text, con);
+
+                cmd.CommandType = CommandType.Text;
+
+                con.Open();
+
+                cmd.ExecuteNonQuery();
+
+                Label1.Text = "Data has been Deleted";
+
+                con.Close();
+
+                TextBox1.Text = "";
+
+                TextBox2.Text = "";
+
+                TextBox3.Text = "";
+
+                TextBox4.Text = "";
+
+            }
+
+            catch
+
+            {
+
+                Label1.Text = " invalid Please selct ID First ";
+
+            }
+
+
+
+        }
+
+        protected void btnviews_Click(object sender, EventArgs e)
+        {
+            
+
+            SqlDataAdapter Adp = new SqlDataAdapter("select * from Employee1", con);
+
+            //DataTable Dt = new DataTable();
+
+            DataSet ds = new DataSet();
+
+            Adp.Fill(ds);
+
+            GridView1.DataSource = ds;
+
+            GridView1.DataBind();
+        }
+        protected void btnsearch_Click(object sender, EventArgs e)
+        {
+           
+
+
+
+            SqlCommand cmd = new SqlCommand("Select * from Employee1 where id=" + TextBox1.Text, con);
+
+            cmd.CommandType = CommandType.Text;
+
+
+
+            SqlDataAdapter da = new SqlDataAdapter();
+
+            da.SelectCommand = cmd;
+
+            DataSet ds = new DataSet();
+
+            da.Fill(ds);
+
+            if (ds.Tables[0].Rows.Count > 0)
+
+            {
+
+                Label2.Text = "Id is Found Successfully";
+
+                Label1.Text = "";
+
+                TextBox2.Text = ds.Tables[0].Rows[0]["name"].ToString();
+
+                TextBox3.Text = ds.Tables[0].Rows[0]["salary"].ToString();
+
+                TextBox4.Text = ds.Tables[0].Rows[0]["address"].ToString();
+
+                btnupdate.Enabled = true;
+
+
+
+            }
+
+            else
+
+            {
+
+                Label2.Text = "No Particular Searched id  Found";
+
+            }
+
+            con.Close();
+
+
+
+        }
+      }
+    }
+
